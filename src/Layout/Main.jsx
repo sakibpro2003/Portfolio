1b3f03d9b10b6afcf7db52756bbc8db4b7b99fbb
Sakib Prodhan <sakibprodhan2003@gmail.com>
2024-10-08 23:01:09 +0600

    navbar elemets update

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
-            <Footer></Footer>
+            {/* <Footer></Footer> */}
         </div>
     );
 };
