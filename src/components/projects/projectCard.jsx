a9edf610f023fe36024073783b7eb4e4f1298fa7
sakibpro2003 <sakibprodhan2003@gmail.com>
2025-05-11 02:33:20 +0600

    fix: project details page UI improved

--- src/components/projects/projectCard.jsx
+++ src/components/projects/projectCard.jsx
@@ -1,14 +1,9 @@
+// src/components/projects/ProjectCard.jsx
 import "./styles/projectCardstyle.css";
 
-const ProjectCard = ({
-	image,
-	title,
-	description,
-	githubLink,
-	detailsLink,
-}) => {
+const ProjectCard = ({ image, title, description, githubLink, detailsLink }) => {
 	return (
-		<div className="project-card">
+		<div className="project-card animated-fade-in-up">
 			<div className="project-card-image-wrapper">
 				<img src={image} alt={title} className="project-card-image" />
 			</div>
@@ -16,18 +11,10 @@ const ProjectCard = ({
 				<h3 className="project-card-title">{title}</h3>
 				<p className="project-card-description">{description}</p>
 				<div className="project-card-buttons">
-					<a
-						href={githubLink}
-						className="project-card-button preview-button"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
+					<a href={githubLink} className="project-card-button details-button" target="_blank" rel="noopener noreferrer">
 						Live Preview
 					</a>
-					<a
-						href={detailsLink}
-						className="project-card-button preview-button"
-					>
+					<a href={detailsLink} className="project-card-button details-button">
 						Details
 					</a>
 				</div>
